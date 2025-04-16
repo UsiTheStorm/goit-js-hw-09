ce8a9e6c6f8bf7f0df6abf5b78e802e256c5a7bf
UsiTheStorm <50480382+UsiTheStorm@users.noreply.github.com>
2025-04-16 13:27:49 +0000

    Deploying to gh-pages from @ UsiTheStorm/goit-js-hw-09@ce58b9b55b580224c1d500ed0d60fc57d2a08903 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{G as o}from"./assets/vendor-CQccJRom.js";o({selector:".glightbox"});
+//# sourceMappingURL=1-gallery.js.map
