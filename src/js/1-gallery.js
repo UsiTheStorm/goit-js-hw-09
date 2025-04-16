b75e0bbc552eb638c569e55c70fe35c7ff528ced
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-04-16 15:26:27 +0100

    Add gallery layout

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,4 +1,27 @@
 import GLightbox from 'glightbox';
 import 'glightbox/dist/css/glightbox.css';
 
+import { galleryItems } from './gallery-items';
+
 const lightbox = GLightbox({ selector: '.glightbox' });
+
+let gallery = document.querySelector('.gallery');
+
+function createMarkup() {
+    let markup = '';
+    galleryItems.forEach(({ preview, original, description }) => {
+        const content = /* html */ `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+      </a>
+      </li>`;
+        markup += content;
+    });
+    gallery.insertAdjacentHTML('beforeend', markup);
+}
+createMarkup();
