684117607fb9f52d34a8a8f625c995924e9b3318
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-04-17 11:16:50 +0100

    Change preview links

--- src/js/gallery-items.js
+++ src/js/gallery-items.js
@@ -1,46 +1,46 @@
 export const galleryItems = [
     {
-        preview: 'https://cdn.pixabay.com/photo/2023/10/06/08/16/lavender-8297714__340.jpg',
+        preview: 'https://cdn.pixabay.com/photo/2023/10/06/08/16/lavender-8297714_640.jpg',
         original: 'https://cdn.pixabay.com/photo/2023/10/06/08/16/lavender-8297714_1280.jpg',
         description: 'Purple flowers',
     },
     {
-        preview: 'https://cdn.pixabay.com/photo/2022/10/28/08/25/amethyst-7552587__340.jpg',
+        preview: 'https://cdn.pixabay.com/photo/2022/10/28/08/25/amethyst-7552587_640.jpg',
         original: 'https://cdn.pixabay.com/photo/2022/10/28/08/25/amethyst-7552587_1280.jpg',
         description: 'Amethyst Crystal',
     },
     {
-        preview: 'https://cdn.pixabay.com/photo/2018/08/18/16/38/night-view-3615087_1280.jpg',
+        preview: 'https://cdn.pixabay.com/photo/2018/08/18/16/38/night-view-3615087_640.jpg',
         original: 'https://cdn.pixabay.com/photo/2018/08/18/16/38/night-view-3615087_1280.jpg',
         description: 'Alley image',
     },
     {
-        preview: 'https://cdn.pixabay.com/photo/2017/08/10/00/40/stars-2616537__340.jpg',
+        preview: 'https://cdn.pixabay.com/photo/2017/08/10/00/40/stars-2616537_640.jpg',
         original: 'https://cdn.pixabay.com/photo/2017/08/10/00/40/stars-2616537_1280.jpg',
         description: 'Night sky',
     },
     {
-        preview: 'https://cdn.pixabay.com/photo/2025/03/18/17/03/dog-9478487__340.jpg',
+        preview: 'https://cdn.pixabay.com/photo/2025/03/18/17/03/dog-9478487_640.jpg',
         original: 'https://cdn.pixabay.com/photo/2025/03/18/17/03/dog-9478487_1280.jpg',
         description: 'Dog on field',
     },
     {
-        preview: 'https://cdn.pixabay.com/photo/2023/04/19/19/11/lake-7938396__340.jpg',
+        preview: 'https://cdn.pixabay.com/photo/2023/04/19/19/11/lake-7938396_640.jpg',
         original: 'https://cdn.pixabay.com/photo/2023/04/19/19/11/lake-7938396_1280.jpg',
         description: 'Cherry Blossoms',
     },
     {
-        preview: 'https://cdn.pixabay.com/photo/2022/02/27/17/03/stones-7037638__340.jpg',
+        preview: 'https://cdn.pixabay.com/photo/2022/02/27/17/03/stones-7037638_640.jpg',
         original: 'https://cdn.pixabay.com/photo/2022/02/27/17/03/stones-7037638_1280.jpg',
         description: 'Balanced stones',
     },
     {
-        preview: 'https://cdn.pixabay.com/photo/2023/06/28/18/00/children-8094952__340.jpg',
+        preview: 'https://cdn.pixabay.com/photo/2023/06/28/18/00/children-8094952_640.jpg',
         original: 'https://cdn.pixabay.com/photo/2023/06/28/18/00/children-8094952_1280.jpg',
         description: 'Kids',
     },
     {
-        preview: 'https://cdn.pixabay.com/photo/2022/07/24/13/14/lavender-7341619__340.jpg',
+        preview: 'https://cdn.pixabay.com/photo/2022/07/24/13/14/lavender-7341619_640.jpg',
         original: 'https://cdn.pixabay.com/photo/2022/07/24/13/14/lavender-7341619_1280.jpg',
         description: 'Lavender flowers',
     },
